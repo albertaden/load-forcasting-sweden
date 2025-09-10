e70a03fed0de17e1d7c985a2ad63b3bd8fbbaae7
albertaden <albert.aden@hotmail.se>
2025-09-10 18:26:13 +0200

    Added comments about plots

--- src/plotting.py
+++ src/plotting.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go  # if you ever want finer control
 from config import TZ, COUNTRY_CODE, DAYS_BACK, SITE_TITLE, SITE_TAGLINE, OUTPUT_DIR, OUTPUT_FILE
 
 
-# -------------- Figures -------------------
+# The load shown a specific hour is the average load over that hour: 
+# e.g the load shown at 01:00 is the average load from 01:00 to 01:59
 def make_actual_load_plot(df: pd.DataFrame, title: str):
     fig = px.line(df, x="Date", y="Load (MW)", title=title, template="plotly_white")
     fig.update_layout(
@@ -24,9 +24,12 @@ def make_actual_load_plot(df: pd.DataFrame, title: str):
         yaxis=dict(title="Load (MW)"),
         margin=dict(l=60, r=30, t=60, b=40),
     )
+    
     return fig
 
+#TODO: Should look into removing/ changing this, not sure if daily avg is useful
 def make_daily_avg_bar_plot(df: pd.DataFrame, title: str):
+    
     # Aggregate hourly → daily average
     daily = (
         df.assign(day=df["Date"].dt.floor("D"))
@@ -41,11 +44,14 @@ def make_daily_avg_bar_plot(df: pd.DataFrame, title: str):
         title=title,
         template="plotly_white",
     )
+    
     fig.update_layout(
         hovermode="x unified",
         xaxis=dict(title="Date"),
         yaxis=dict(title="Daily average load (MW)"),
         margin=dict(l=60, r=30, t=60, b=40),
     )
+    
     fig.update_traces(hovertemplate="Date: %{x|%Y-%m-%d}<br>Daily avg: %{y:.0f} MW<extra></extra>")
+    
     return fig
